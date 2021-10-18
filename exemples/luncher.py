9f2d93b73d4e227b7f96db70e7e8e88ecd7080a3
4surix <asurix@outlook.fr>
2021-10-18 10:52:15 +0200

    Update exemple

---
+++ exemples/luncher.py
@@ -0,0 +1,6 @@
+from pitwi import parser
+
+
+root = parser.file('NAME FILE.xml') # for exemple : calcul.xml
+
+root.run()
--- exemples/weather.py
+++
@@ -1,5 +0,0 @@
-from pitwi import parser
-
-root = parser.file('weather.xml')
-
-root.run()
